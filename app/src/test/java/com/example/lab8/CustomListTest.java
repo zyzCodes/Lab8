e40911d9c93e6c5e9a13bd74f7ce423c415c13eb
UncleSamTech <samueloel3486@gmail.com>
2022-10-26 14:05:11 -0600

    removed the test implementation to enable use by other labs

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -11,25 +11,6 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
-    private CustomList list;
 
-    /**
-     * create a mocklist for my citylist
-     * @return
-     */
-    public CustomList MockCityList(){
-    list =  new CustomList(null,new ArrayList<>());
-    return list;
-}
-
-
-    @Test
-    public void addCityTest(){
-        list = MockCityList();
-        int listSize = list.getCount();
-        list.addCity(new City("Estevan", "SM"));
-        assertEquals(list.getCount(),listSize + 1);
-
-    }
 
 }

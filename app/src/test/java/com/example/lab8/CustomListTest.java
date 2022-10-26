7f8a19caa9e28a7452eec44339cfb635902ecc4c
UncleSamTech <samueloel3486@gmail.com>
2022-10-26 12:14:01 -0600

    check

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -4,7 +4,8 @@ package com.example.lab8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -12,6 +13,10 @@ import java.util.ArrayList;
 public class CustomListTest {
     private CustomList list;
 
+    @Before
+    public void createList(){
+        list = new CustomList(null, new ArrayList<City>());
+    }
 
     @Test
     public void addCityTest(){

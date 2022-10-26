9238735d2fdf6eea54d6083f5ad7fc4be6c323e6
UncleSamTech <samueloel3486@gmail.com>
2022-10-26 13:15:13 -0600

    refactor

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -4,7 +4,7 @@ package com.example.lab8;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-import org.junit.Before;
+//import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -13,13 +13,15 @@ import java.util.ArrayList;
 public class CustomListTest {
     private CustomList list;
 
-    @Before
-    public void createList(){
-        list = new CustomList(null, new ArrayList<City>());
-    }
+
+public CustomList MockCityList(){
+    list =  new CustomList(null,new ArrayList<>());
+    return list;
+}
 
     @Test
     public void addCityTest(){
+        list = MockCityList();
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SM"));
         assertEquals(list.getCount(),listSize + 1);

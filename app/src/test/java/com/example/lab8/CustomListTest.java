489f57d39a0ca2fa7dd3869af2727634403db49f
UncleSamTech <samueloel3486@gmail.com>
2022-10-26 11:44:24 -0600

    checking failed test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -1,19 +1,20 @@
 package com.example.lab8;
 
-import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
     private CustomList list;
 
-    @Before
-    public void createList(){
-        list = new CustomList(null, new ArrayList<City>());
-    }
+   // @Before
+    //public void createList(){
+       // list = new CustomList(null, new ArrayList<City>());
+   // }
 
     @Test
     public void addCityTest(){
